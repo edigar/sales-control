ec01be372e4670ec3dc5c76d4307ffdc42fbb3bc
Edigar Ribeiro <edigarhdev@gmail.com>
2025-10-26 21:04:13 -0300

    style: remove unnecessary comment

--- tests/Unit/Services/Sale/SaleServiceTest.php
+++ tests/Unit/Services/Sale/SaleServiceTest.php
@@ -40,8 +40,6 @@ class SaleServiceTest extends TestCase
         parent::tearDown();
     }
 
-    // ==================== Helper Methods ====================
-
     private function mockDbTransaction(): void
     {
         DB::shouldReceive('transaction')
